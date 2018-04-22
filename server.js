d61de1fe8e348bb5c5ccfdd25948bce451f12c21
maoyang <my@esast.com>
2018-04-22 22:55:45 +0800

    git init

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+var app = express();
+
+const hbs = require('hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req, res) => {
+    //res.send('<h1>Hello Express!</h1>');
+    
+    res.render('home.hbs',{
+        title: 'My Home',
+        pageTitle: 'This is MY homepage',
+        message: 'Welcome to my home',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/about',(req,res) => {
+   res.render('about.hbs',{
+       pageTitle: 'About Page',
+       currentYear: new Date().getFullYear()
+   });
+});
+
+app.get('/bad',(req,res) => {
+    res.send({
+       errorMessage: 'No Data'
+    });
+});
+
+app.listen(3000,()=>{
+    console.log('Server listen on 30000');
+});
