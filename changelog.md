7ac634ef7493c99f0a966bd7090e5fa4409db292
maoyang <my@esast.com>
2018-05-01 01:35:11 +0800

    Setup start script and heroku port

--- changelog.md
+++ changelog.md
@@ -2,3 +2,5 @@
 
 # html template 共用 html 標籤 partials 
 # partials 可以定義 helper function 
+
+進度 : Lecture 45 Express 中介軟體
--- server.js
+++ server.js
@@ -3,12 +3,38 @@ const express = require('express');
 var app = express();
 
 const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials'); 
 
 app.set('view engine', 'hbs');
 
+
+
+
+//所有的 request 都會經過這個 middleware , 如果沒有呼叫 next , server 無法將執行 request path 
+app.use((req,res,next) => {
+   var now = new Date().toString();
+   var log = `${now}: ${req.method} ${req.url}`;
+   console.log(log);
+   fs.appendFile('server.log',log + '\n',(err) => {
+       if (err){
+           console.log('Unable to append to server.log.');
+       }
+   });
+   next();
+  
+
+});
+
+// app.use((req,res,next) => {
+//     res.render('maintain.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
+
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
@@ -41,6 +67,6 @@ app.get('/bad',(req,res) => {
     });
 });
 
-app.listen(3000,()=>{
-    console.log('Server listen on 30000');
+app.listen(port,()=>{
+    console.log(`Server listen on ${port}`);
 });
