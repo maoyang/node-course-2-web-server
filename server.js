f4a356ed650fbde299e611c1f204ebe6ef76b4be
maoyang <my@esast.com>
2018-05-01 23:41:46 +0800

    adding project link

--- server.js
+++ server.js
@@ -55,6 +55,15 @@ app.get('/',(req, res) => {
     });
 });
 
+app.get('/project',(req,res) => {
+     res.render('project.hbs',{
+         title: 'My Projects',
+         pageTitle: 'Project list',
+         message: 'Hello My Pofolio'
+     });
+
+});
+
 app.get('/about',(req,res) => {
    res.render('about.hbs',{
        pageTitle: 'About Page'
